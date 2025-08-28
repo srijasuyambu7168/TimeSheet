14bc3487a4f366f058b5ae940deacce79c4d013a
srijasuyambu7168 <srija7168@gmail.com>
2025-08-29 00:33:43 +0530

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace TimesheetApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
