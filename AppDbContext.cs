99822d826f54a956f918d6430cf30499af741c74
srijasuyambu7168 <srija7168@gmail.com>
2025-09-02 19:15:08 +0530

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+using Microsoft.EntityFrameworkCore;
+using TimesheetApp.Models;
 
 namespace TimesheetApp.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext()
-        { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Timesheet> Timesheets { get; set; }
     }
 }
